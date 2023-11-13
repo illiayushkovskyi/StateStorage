9ae6f8189b9a3d791a481da733541d5ea868dde7
Illia Yushkovskyi <haishtipil@gmail.com>
2023-11-13 19:05:28 +0100

    Добавьте файлы проекта.

---
+++ StateStorage/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StateStorage.Models;
+using System.Diagnostics;
+
+namespace StateStorage.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(DataModel model)
+        {
+            CookieOptions ending = new CookieOptions();
+            ending.Expires = model.End;
+            Response.Cookies.Append("Data", model.Adress, ending);
+            return View();
+        }
+
+        public IActionResult Privacy()
+
+        {
+            if (Request.Cookies.TryGetValue("Data", out string value))
+            {
+                return View((object)value);
+
+            }
+            return View((object)"Absent");
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ StateStorage/Models/DataModel.cs
@@ -0,0 +1,9 @@
+﻿namespace StateStorage.Models
+{
+    public class DataModel
+    {
+        public string Adress { get; set; }
+        public DateTime End { get; set; }
+
+    }
+}
